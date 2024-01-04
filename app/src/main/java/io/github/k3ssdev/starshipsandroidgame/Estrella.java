e270d42032a446ae6a5505cbaa1209de4045c884
Alberto Pérez <albertoperezdelrio@gmail.com>
2024-01-04 10:22:27 +0100

    Update Estrella.java

--- app/src/main/java/io/github/k3ssdev/starshipsandroidgame/Estrella.java
+++ app/src/main/java/io/github/k3ssdev/starshipsandroidgame/Estrella.java
@@ -11,6 +11,7 @@ public class Estrella {
     private float x2;
     private final float y2;
 
+    // Constructor que inicializa las coordenadas de la estrella
     public Estrella(float x1, float y1, float x2, float y2) {
         this.x1 = x1;
         this.y1 = y1;
@@ -18,27 +19,32 @@ public class Estrella {
         this.y2 = y2;
     }
 
+    // Método para obtener la coordenada x1 de la estrella
     public float getX1() {
         return x1;
     }
 
+    // Método para obtener la coordenada y1 de la estrella
     public float getY1() {
         return y1;
     }
 
+    // Método para obtener la coordenada x2 de la estrella
     public float getX2() {
         return x2;
     }
 
+    // Método para obtener la coordenada y2 de la estrella
     public float getY2() {
         return y2;
     }
 
+    // Método para mover la estrella en el eje x
     public void mover() {
         x1 -= 2;  // Ajusta la velocidad de las estrellas
         x2 -= 2;
 
-        // Asegúrate de que las estrellas vuelvan a aparecer cuando se salgan de la pantalla
+        // Las estrellas vuelvan a aparecer cuando se salgan de la pantalla
         if (x1 < 0) {
             x1 = Juego.ancho;
             x2 = x1 + random.nextInt(5) - 2;
